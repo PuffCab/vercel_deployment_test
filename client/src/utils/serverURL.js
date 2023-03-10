3f06d19fcc6c9f3215f9c3c944a1ef6443b8a431
PuffCab <berlinkollner@gmail.com>
2023-03-10 01:48:59 +0100

    serverURL corrected

--- client/src/utils/serverURL.js
+++ client/src/utils/serverURL.js
@@ -1,6 +1,6 @@
 const serverURL =
   process.env.NODE_ENV === "production"
-    ? "https://vercel-front-app.vercel.app/"
+    ? "https://vercel-front-app.vercel.app"
     : "http://localhost:5005";
 
 export { serverURL };
